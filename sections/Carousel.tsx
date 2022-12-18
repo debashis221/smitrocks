1c5bbbd870a92317303ab5ed4b23ff0681bfe062
Debashis saha <debashissaha221@gmail.com>
2022-12-19 04:53:09 +0530

    chnages

--- sections/Carousel.tsx
+++ sections/Carousel.tsx
@@ -38,6 +38,7 @@ const Carousel = () => {
           <Image
             src="https://c4.wallpaperflare.com/wallpaper/808/87/756/son-goku-dragon-ball-ultra-instinct-dragon-ball-super-white-hair-hd-wallpaper-preview.jpg"
             alt="image"
+            fill
             width={1100}
             height={600}
           />

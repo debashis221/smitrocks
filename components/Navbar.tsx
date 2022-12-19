53fe220205158ec237b5661242e3f2cb8e609d61
Debashis saha <debashissaha221@gmail.com>
2022-12-19 19:20:58 +0530

    changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function NavBar() {
           <div className="flex flex-col w-full border-opacity-50">
             <div className="grid card bg-base-200 rounded-box place-items-center lg:py-3 lg:px-5">
               <form className="place-items-center">
-                <div className="py-3 px-5">
+                <div className="">
                   <label className="input-group">
                     <span>
                       <FiMail />
@@ -30,7 +30,7 @@ export default function NavBar() {
                     />
                   </label>
                 </div>
-                <div className="px-5">
+                <div className="">
                   <label className="input-group">
                     <span>
                       <FaLock />

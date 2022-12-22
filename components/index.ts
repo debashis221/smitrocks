f8b84922f091879fc869a9c2c52aace94f86f633
Debashis saha <debashissaha221@gmail.com>
2022-12-22 18:34:04 +0530

    chnages

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,5 @@ import NavBar from "./Navbar";
 import NoticeItem from "./NoticeItem";
 import CourseItem from "./CourseItem";
 import TestimonialsItem from "./TestimonialsItem";
-import Footer from "./Footer";
+import { Footer } from "./Footer";
 export { NavBar, NoticeItem, CourseItem, TestimonialsItem, Footer };

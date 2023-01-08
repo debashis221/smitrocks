a11499bdbcc436704bebff960ff259f4dcd1ea85
Debashis saha <debashissaha221@gmail.com>
2023-01-08 18:09:01 +0530

    chnages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -156,7 +156,7 @@ export default function NavBar() {
                   <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                 </svg>
               </a>
-              <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+              <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box">
                 <li>
                   <a>Submenu 1</a>
                 </li>

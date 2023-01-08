2fe2cc85e9edbe6aaa9dcb292cb3a215f6fdf4c8
Debashis saha <debashissaha221@gmail.com>
2023-01-08 18:06:34 +0530

    chnages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -209,14 +209,14 @@ export default function NavBar() {
                 tabIndex={0}
                 className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
               >
-                {userData!.data.user.isAdmin ? (
+                {userData && userData!.data.user.isAdmin ? (
                   <li>
                     <a>Admin Panel</a>
                   </li>
                 ) : (
                   <></>
                 )}
-                {userData!.data.user.isTeacher ? (
+                {userData && userData!.data.user.isTeacher ? (
                   <li>
                     <a>Faculty Panel</a>
                   </li>

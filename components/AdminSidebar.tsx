eef78f904213bbf97e5c8bc20a01bd70916fb1f2
Debashis Saha <debashissaha221@gmail.com>
2023-01-17 16:11:28 +0530

    chnages

--- components/AdminSidebar.tsx
+++ components/AdminSidebar.tsx
@@ -2,208 +2,88 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RxCross1 } from "react-icons/rx";
-import { BsFilterLeft } from "react-icons/bs";
+import { BsFilterLeft, BsHouse } from "react-icons/bs";
 import AdminNavBar from "./AdminNavBar";
+import Link from "next/link";
+import { fetchSingleUser, User } from "../axios/services/users.service";
 
 const AdminSidebar = () => {
   const { data: session, status } = useSession();
   const [open, setOpen] = useState(false);
+  const [userData, setUserData] = useState<User>();
   const router = useRouter();
-  if (typeof window === "undefined") return null;
-  if (status === "loading") {
-    return <p className="text-center">Loading...</p>;
+  type menuProps = {
+    name: string;
+    href: string;
+    icon: JSX.Element;
+  };
+  let menu: menuProps[] = [];
+  async function getUserData() {
+    if (session) {
+      const user = await fetchSingleUser(session!.user!.email!);
+      setUserData(user);
+    }
   }
-  if (status === "unauthenticated") {
-    router.push("/");
-  } else {
-    return (
-      <div className="overflow-y-hidden">
-        <AdminNavBar session={session} />
-        <span
-          className="absolute text-white text-4xl top-5 left-4 cursor-pointer"
-          onClick={() => setOpen(!open)}
-        >
-          <BsFilterLeft className="px-2 bg-[#13005A] rounded-md" />
-        </span>
-        <div
-          className={`sidebar fixed top-0 bottom-0 lg:left-0 ${
-            open ? "" : "left-[-300px]"
-          }  duration-1000
+  useEffect(() => {
+    getUserData();
+  }, [session]);
+
+  if (userData?.data.user.isAdmin) {
+    menu = [
+      {
+        name: "Home",
+        href: "/admin",
+        icon: <BsHouse />,
+      },
+    ];
+  }
+
+  return (
+    <div className="overflow-y-hidden">
+      <AdminNavBar session={session} />
+      <span
+        className="absolute text-white text-4xl top-5 left-4 cursor-pointer"
+        onClick={() => setOpen(!open)}
+      >
+        <BsFilterLeft className="px-2 bg-[#13005A] rounded-md" />
+      </span>
+      <div
+        className={`sidebar fixed top-0 bottom-0 lg:left-0 ${
+          open ? "" : "left-[-300px]"
+        }  duration-1000
     p-2 w-[300px] overflow-y-auto text-center bg-[#13005A] shadow h-screen`}
-        >
-          <div className="text-gray-100 text-xl">
-            <div className="p-2.5 mt-1 flex items-center rounded-md ">
-              <h1 className="text-[15px]  ml-3 text-xl text-gray-200 font-bold">
-                SMIT ROCKS
-              </h1>
-              <RxCross1
-                className="ml-28 cursor-pointer lg:hidden"
-                onClick={() => setOpen(!open)}
-              />
-            </div>
-            <hr className="my-2 text-gray-600" />
-            <ul className="menu bg-base-100 w-56 p-2 rounded-box">
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                  </svg>
-                  Item 2
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  Item 1
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Item 3
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Item 3
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Item 3
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Item 3
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Item 3
-                </a>
-              </li>
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Item 3
-                </a>
-              </li>
+      >
+        <div className="text-gray-100 text-xl">
+          <div className="p-2.5 mt-1 flex items-center rounded-md ">
+            <h1 className="text-[15px]  ml-3 text-xl text-gray-200 font-bold">
+              SMIT ROCKS
+            </h1>
+            <RxCross1
+              className="ml-28 cursor-pointer lg:hidden"
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+          <hr className="my-2 text-gray-600" />
+          <div className="flex flex-col items-center justify-center">
+            <ul className="menu bg-base-100 w-60 p-2 rounded-box my-5">
+              {menu.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <Link href="/admin">
+                      <BsHouse size={25} />
+                      Home
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
       </div>
-    );
-  }
-  return <></>;
+    </div>
+  );
 };
 
 export default AdminSidebar;

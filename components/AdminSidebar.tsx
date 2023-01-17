61a499c4d4ac3ceb9b90248e2e4a89e4767a0b8b
Debashis Saha <debashissaha221@gmail.com>
2023-01-17 11:03:19 +0530

    changes

--- components/AdminSidebar.tsx
+++ components/AdminSidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { fetchSingleUser } from "axios/services/users.service";
+import { fetchSingleUser } from "../axios/services/users.service";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
